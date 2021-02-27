2d49c74605e7ddc91f672c2a8095406188d1c68a
zuramai <ahmadsaugigis@yahoo.com>
2021-02-28 06:39:20 +0700

    update landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,48 @@
 <template>
     <div class='main-layout'>
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img class = "toplogoimg" src="@/assets/images/logo/logo.png" alt="Shape of a head and body with a text beside saying HANAbook">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
         <Nuxt/>
+
+        
+        <section class = "footer">
+            <div class="container">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="copyright">
+                        <p class='mb-0'>&copy; Copyright by Fishy Duck Services</p>
+                    </div>
+                    <div class = "socials">
+                        <a href=""><i class="bi bi-facebook"></i></a>
+                        <a href=""><i class="bi bi-instagram"></i></a>
+                        <a href=""><i class="bi bi-twitter"></i></a>
+                    </div> 
+                </div>
+            </div>
+        </section>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,30 @@
 <template>
-
     <div>
 
-    <nav>
-        <div class="logo">
-            <img class = "toplogoimg" src="@/assets/images/logo/logo.png" alt="Shape of a head and body with a text beside saying HANAbook">
-        </div>
-
-        <ul class = "nav-links">
-            <li><a href = '#'>Home</a></li>
-            <li><a href = '#'>About Us</a></li>
-            <li><a href = '#'>Features</a></li>
-            <li><a href = '#'>Contact Us</a></li>
-        </ul>
-
-        <div class="burger">
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-        </div>
-    </nav>
 
 
     <section class="hero">
         <div class="container">
-            <div class="left-col">
-                <div class="hero-cta">
+            <div class="row">
+                <div class="col-lg-7 col-12">
+                    <h1>Appoint Your Vaccination Schedule with HANAbook</h1>
+                    <p class='fs-4'>Get your appointment schedule at the closest clinic by only a click.</p>
 
-                    <div class="imgcontainer">
-
-                        <img class = "hana" src="@/assets/images/logo/just-logo.png" alt="Blue head purple body">
-
-                        <img src="@/assets/images/bg/covidinject.jpg" class="hero-img" id = "herobro" alt="Doc giving someone an injection">
-
-                        <div class="buttons">
-                            <button type = "button" class = "buttontxt leftbtn">Register as a vaccine participant</button>
-                            <button type = "button" class = "buttontxt rightbtn">Register your clinic</button>
-                        </div>
-                        
+                    <div class="buttons-cta">
+                        <nuxt-link :to="{name: 'auth-register'}" class="btn btn-primary shadow">Register Now</nuxt-link>
                     </div>
-
+                </div>
+                <div class="col-lg-5 col-12">
+                    <div>
+                        <img src="@/assets/images/bg/vector.png" id="hero-vector" alt="Vaccination illustration">
+                    </div>
                 </div>
             </div>
         </div>
     </section>
-
     <section class="features-section">
         <div class="container">
-            <h2 class = "featuretxt">Features</h2>
+            <h1 class="mb-5">Features</h1>
             <ul class="features-list">
                 <li>Find out if you are eligible for the vaccine</li>
                 <li>Book your vaccine appointment as soon as possible</li>
@@ -61,70 +38,49 @@
     </section>
 
     <section class="contact-section v2cont">
-        <div class="container navcontainer">
-            <div class="contact-left">
-                <h2>Contact us if you have trouble booking a vaccine appointment.</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-12">
+                    <iframe class = "map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.8113900679374!2d103.68173161688698!3d1.3473190376878703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da0f0a99014463%3A0xb8bb0800c52d8219!2sNanyang%20Technological%20University!5e0!3m2!1sen!2smy!4v1614425870333!5m2!1sen!2smy" width="400" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+                <div class="col-lg-6 offset-lg-1 col-12">
+                    <h1>Contact us</h1>
+                    <p class='fs-4'>If you have trouble booking a vaccine appointment, we will always help you.</p>
 
-                <form action="">
-                    <label for="name">Name</label>
-                    <input type="text" id="name" name="name">
+                    <form action="">
+                        <div class="mb-3">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" name="name" class='form-control'>
+                        </div>
+                        <div class="mb-3">
+                            <label for="number">Phone Number</label>
+                            <input type="text" id="number" name="number" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="5"  class="form-control"></textarea>
+                        </div>
 
-                     <label for="number">Phone Number</label>
-                    <input type="text" id="number" name="number">
-
-                    <label for="message">Message</label>
-                    <textarea name="message" id="message" cols="30" rows="5"></textarea>
-
-                    <input type="submit" class="sendmsg" value="Send message">
-                </form>
-            </div>
-            <div class = "verticalcont">
-                <iframe class = "map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.8113900679374!2d103.68173161688698!3d1.3473190376878703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da0f0a99014463%3A0xb8bb0800c52d8219!2sNanyang%20Technological%20University!5e0!3m2!1sen!2smy!4v1614425870333!5m2!1sen!2smy" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        <input type="submit" class="btn btn-primary float-end" value="Send message" >
+                    </form>
+                </div>
             </div>
 
         </div>
     </section>
 
-    <section class = "footer">
-
-            <div class="logofooter">
-                <img class = "footerlogoimg" src="@/assets/images/logo/logo.png" alt="Shape of a head and body with a text beside saying HANAbook">
-            </div>
-
-            <div class="copyright">
-                <p>(C) Copyright by Fishy Duck Services</p>
-            </div>
-
-            <div class = "socials">
-                <a target="_blank" href="#"><img class = "smallimg" src="@/assets/images/socialmedia/facebook.svg"></a>
-                <a target="_blank" href="#"><img class = "smallimg" src="@/assets/images/socialmedia/insta.svg"></a>
-                <a target="_blank" href="#"><img class = "smallimg" src="@/assets/images/socialmedia/twitter.svg"></a>
-            </div> 
-
-        </section>
-
-
-    </div>
+</div>
 
 </template>
 
-<style>
+<style lang="scss">
     @import "@/assets/scss/pages/landing.scss";
 </style>
 
 <script>
 export default {
     mounted() {
-        const fadeinNavMenu = () => {
-            const burger = document.querySelector(".burger");
-            const nav = document.querySelector(".nav-links");
 
-            burger.addEventListener('click', () => {
-               nav.classList.toggle("nav-active"); 
-            });
-        }
-
-        fadeinNavMenu();
     }
 }
 </script>
